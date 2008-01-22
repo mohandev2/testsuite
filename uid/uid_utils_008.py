7b629a458968c86e49405e58795de87e7f107c46
jayshree_tp <jayshree_tp@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-01-22 14:27:31 +0000

    added the copyright headers

--- uid/uid_utils_008.py
+++ uid/uid_utils_008.py
@@ -1,10 +1,30 @@
+"""
+ (C) Copyright IBM Corp. 2008
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+  file and program are licensed under a BSD style license.  See
+  the Copying file included with the OpenHPI distribution for
+  full licensing terms.
+ 
+ Authors:
+    Jayashree Padmanabhan <jayshree@in.ibm.com>
+"""
+
 #!/usr/bin/env python
 
 from openhpi import *
 import unittest
 
 class TestSequence(unittest.TestCase):
-       
+    """
+    runTest : Calls oh_uid_from_entity_path using a NULL entity path. 
+    Passes if it returns 0,
+    otherwise fails.
+
+    Return value: 0 on success, 1 on failure
+    """
     def runTest(self):
                 
         self.assertEqual(oh_uid_initialize(),0)
