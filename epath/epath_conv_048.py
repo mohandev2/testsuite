1ca73be2ddc85e18e2f24212b4d71b3c1ea71f2b
suntrupth <suntrupth@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-04-03 11:46:05 +0000

    Entity path unit test cases added

---
+++ epath/epath_conv_048.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""
+ (C) Copyright IBM Corp. 2008
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ file and program are licensed under a BSD style license.  See
+ the Copying file included with the OpenHPI distribution for
+ full licensing terms.
+ 
+ Authors:
+    Suntrupth S Yadav <suntrupth@in.ibm.com>
+"""
+
+import unittest
+from openhpi import *
+
+class TestSequence(unittest.TestCase):
+
+    """
+     main: 
+     epathstr -> epath test
+     
+     Test if an entity path string is converted properly into an entity path.
+    """
+    def runTest(self):
+        test_string = ("{PROCESSOR,17}{COMPACTPCI_CHASSIS,33}")
+        bigbuf=oh_big_textbuffer()
+        ep=SaHpiEntityPathT()
+        err = oh_encode_entitypath(test_string, ep)
+        
+        self.assertEqual (err!=None,True)
+        
+        self.assertEqual (ep.Entry[0].EntityType != SAHPI_ENT_COMPACTPCI_CHASSIS,False)
+        
+        self.assertEqual (ep.Entry[0].EntityLocation != 33,False) 
+        
+        self.assertEqual (ep.Entry[1].EntityType != SAHPI_ENT_PROCESSOR,False) 
+        
+        self.assertEqual (ep.Entry[1].EntityLocation != 17,False) 
+        
+        oh_init_bigtext(bigbuf)
+        err = oh_decode_entitypath(ep, bigbuf)
+        
+        self.assertEqual (err!=None,True)
+        
+        self.assertEqual (bigbuf.Data!= test_string,False)
+        
+if __name__=='__main__':
+    unittest.main()
---
+++ epath/epath_conv_049.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""
+ (C) Copyright IBM Corp. 2008
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ file and program are licensed under a BSD style license.  See
+ the Copying file included with the OpenHPI distribution for
+ full licensing terms.
+ 
+ Authors:
+    Suntrupth S Yadav <suntrupth@in.ibm.com>
+"""
+
+import unittest
+from openhpi import *
+
+class TestSequence(unittest.TestCase):
+    
+    """
+     main: 
+     epathstr -> epath test
+     
+     Test if an entity path string is converted properly into an entity path.
+    """
+    def runTest(self):
+        test_string = ("{OTHER_CHASSIS_BOARD,50}{BACK_PANEL_BOARD,80}")
+        bigbuf=oh_big_textbuffer()
+        ep=SaHpiEntityPathT()
+        err = oh_encode_entitypath(test_string, ep)
+    
+        self.assertEqual (err!=None,True)
+        
+        self.assertEqual (ep.Entry[0].EntityType != SAHPI_ENT_BACK_PANEL_BOARD,False) 
+        
+        self.assertEqual (ep.Entry[0].EntityLocation != 80,False) 
+        
+        self.assertEqual (ep.Entry[1].EntityType != SAHPI_ENT_OTHER_CHASSIS_BOARD,False) 
+        
+        self.assertEqual (ep.Entry[1].EntityLocation != 50,False) 
+        
+        oh_init_bigtext(bigbuf)
+        err = oh_decode_entitypath(ep, bigbuf)
+        
+        self.assertEqual (err!=None,True)
+
+        self.assertEqual (bigbuf.Data!= test_string,False)
+        
+if __name__=='__main__':
+    unittest.main()
