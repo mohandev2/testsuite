93fc80cc7cbd3997240a6965f5e4483cc3d7cd5d
suntrupth <suntrupth@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-04-04 09:23:03 +0000

    Entity Path Tests added

---
+++ epath/epath_conv_090.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""
+ (C) Copyright IBM Corp. 2008
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ file and program are licensed under a BSD style license.  See
+ the Copying file included with the OpenHPI distribution for
+ full licensing terms.
+ 
+ Authors:
+    Suntrupth S Yadav <suntrupth@in.ibm.com>
+"""
+
+import unittest
+from openhpi import *
+
+class TestSequence(unittest.TestCase):
+
+    """
+     main: 
+     epathstr -> epath test
+     
+     Test if an entity path string is converted properly into an entity path.
+    """
+    def runTest(self):
+        test_string = ("{POWER_MODULE,6}{PROCESSOR,65}")
+        bigbuf=oh_big_textbuffer()
+        ep=SaHpiEntityPathT()
+        err = oh_encode_entitypath(test_string, ep)
+        
+        self.assertEqual (err!=None,True)
+        
+        self.assertEqual (ep.Entry[0].EntityType != SAHPI_ENT_PROCESSOR,False)
+        
+        self.assertEqual (ep.Entry[0].EntityLocation != 65,False)
+        
+        self.assertEqual (ep.Entry[1].EntityType != SAHPI_ENT_POWER_MODULE,False)
+        
+        self.assertEqual (ep.Entry[1].EntityLocation != 6,False)
+        
+        oh_init_bigtext(bigbuf)
+        err = oh_decode_entitypath(ep, bigbuf)
+        
+        self.assertEqual (err!=None,True)
+        
+        self.assertEqual (bigbuf.Data!= test_string,False)
+        
+if __name__=='__main__':
+    unittest.main()
+        
