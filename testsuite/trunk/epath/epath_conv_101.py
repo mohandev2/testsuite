59cfe852be344b9153bc0447db42cae6e56a99cc
suntrupth <suntrupth@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2008-04-04 10:09:03 +0000

    Entity Path Tests added

---
+++ testsuite/trunk/epath/epath_conv_101.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+ (C) Copyright IBM Corp. 2008
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ file and program are licensed under a BSD style license.  See
+ the Copying file included with the OpenHPI distribution for
+ full licensing terms.
+ 
+ Authors:
+    Suntrupth S Yadav <suntrupth@in.ibm.com>
+"""
+
+import unittest
+from openhpi import *
+
+class TestSequence(unittest.TestCase):
+
+    """
+     main: 
+     epathstr -> epath test
+     
+     Test if an entity path string is converted properly into an entity path.
+    """
+    def runTest(self):
+        test_string = ("{OEM_SYSINT_SPECIFIC,6}{BOARD_SET_SPECIFIC,30}")
+        ep=SaHpiEntityPathT()
+        err = oh_encode_entitypath(test_string, ep)
+        
+        self.assertEqual (err!=None,True)
+    
+        self.assertEqual (ep.Entry[0].EntityType != SAHPI_ENT_BOARD_SET_SPECIFIC,False)
+        
+        self.assertEqual (ep.Entry[0].EntityLocation != 30,False)
+        
+        self.assertEqual (ep.Entry[1].EntityType != SAHPI_ENT_OEM_SYSINT_SPECIFIC,False)
+        
+        self.assertEqual (ep.Entry[1].EntityLocation != 6,False)
+        
+if __name__=='__main__':
+    unittest.main()
